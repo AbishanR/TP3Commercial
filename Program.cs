894a1985043c1ae81ed71feca2d5df3455005652
AbishanR <159271603+AbishanR@users.noreply.github.com>
2024-03-20 15:42:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TPCommercial
+{
+
+    class Commercial
+    {
+        private string nom;
+        private string prenom;
+        private int anneeNaissance;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int AnneeNaissance { get => anneeNaissance; set => anneeNaissance = value; }
+
+        public Commercial(string Nom, string Prenom, int AnneeNaissance)
+        {
+            this.nom = Nom;
+            this.prenom = Prenom;
+            this.anneeNaissance = AnneeNaissance;
+        }
+
+        public double CalculAge()
+        {
+            return 2024 - this.anneeNaissance;
+        }
+    }
+
+
+}   
